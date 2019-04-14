18b64038f7fa37a168f1c3cc45abb023639073eb
Dhole <dhole@riseup.net>
2019-04-14 13:43:42 +0200

    Add fixed point

--- examples/image_i2c.rs
+++ examples/image_i2c.rs
@@ -23,10 +23,15 @@
 #![no_std]
 #![no_main]
 
+extern crate bit_field;
 extern crate cortex_m;
 extern crate cortex_m_rt as rt;
+extern crate fixed;
 extern crate panic_semihosting;
 extern crate stm32f1xx_hal as hal;
+
+#[macro_use]
+extern crate lazy_static;
 
 use cortex_m_rt::ExceptionFrame;
 use cortex_m_rt::{entry, exception};
@@ -41,32 +46,48 @@ use ssd1306::prelude::*;
 use ssd1306::Builder;
 use stm32f1xx_hal::timer::Timer;
 
-const PI: f32 = 3.14159;
-const PI2: f32 = PI * PI;
-const PI3: f32 = PI2 * PI;
-const PI4: f32 = PI3 * PI;
-const PI5: f32 = PI4 * PI;
+use fixed::frac::U16;
+use fixed::FixedI32;
 
-fn _sin(x: f32) -> f32 {
-    let x3 = x * x * x;
-    let x5 = x3 * x * x;
-    2.0 * PI * x - 4.0 * PI3 * x3 / 3.0 + 4.0 * PI5 * x5 / 15.0
+type F32 = FixedI32<U16>;
+
+use bit_field::BitArray;
+use bit_field::BitField;
+
+macro_rules! F {
+    ($v:expr) => {
+        F32::overflowing_from_float($v).0
+    };
 }
 
-fn sin(x: f32) -> f32 {
-    if x >= 1.0 {
-        sin(x - 1.0)
-    } else if x >= 0.51 {
-        -sin(x - 0.5)
-    } else if x >= 0.26 {
-        sin(0.5 - x)
+lazy_static! {
+    static ref PI: F32 = F!(3.14159);
+    static ref PI2: F32 = *PI * *PI;
+    static ref PI3: F32 = *PI2 * *PI;
+    static ref PI4: F32 = *PI3 * *PI;
+    static ref PI5: F32 = *PI4 * *PI;
+}
+
+fn _sin(x: F32) -> F32 {
+    let x3 = x * x * x;
+    let x5 = x3 * x * x;
+    2 * *PI * x - 4 * *PI3 * x3 / 3 + 4 * *PI5 * x5 / 15
+}
+
+fn sin(x: F32) -> F32 {
+    if x >= 1 {
+        sin(x - F!(1.0))
+    } else if x >= F!(0.51) {
+        -sin(x - F!(0.5))
+    } else if x >= F!(0.26) {
+        sin(F!(0.5) - x)
     } else {
         _sin(x)
     }
 }
 
-fn cos(x: f32) -> f32 {
-    sin(x + 0.25)
+fn cos(x: F32) -> F32 {
+    sin(x + F!(0.25))
     // let x2 = x * x;
     // let x4 = x2 * x * x;
     // let x6 = x4 * x * x;
@@ -92,21 +113,21 @@ fn set_px(xs: &mut [u8], v: u8, x: u32, y: u32, w: u32, h: u32) {
 }
 
 fn rotate(dst: &mut [u8], src: &[u8], w: u32, h: u32, rot: u8) {
-    let theta = 1.0 - (rot as f32) / 255.0;
+    let theta = F!(1.0) - F!(rot as f32) / F!(255.0);
     let a = cos(theta);
     let b = -sin(theta);
     let c = -b;
     let d = a;
-    let (w2, h2) = (w as f32 / 2.0, h as f32 / 2.0);
+    let (w2, h2) = (F!(w as f32) / F!(2.0), F!(h as f32) / F!(2.0));
     for y in 0..h {
-        let _y = y as f32 - h2;
+        let _y = F!(y as f32) - h2;
         let _x0 = b * _y;
         let _y0 = d * _y;
         for x in 0..w {
-            let _x = x as f32 - w2;
+            let _x = F!(x as f32) - w2;
             let x0 = a * _x + _x0;
             let y0 = c * _x + _y0;
-            let v = get_px(src, (x0 + w2) as u32, (y0 + w2) as u32, w, h);
+            let v = get_px(src, (x0 + w2).to_int(), (y0 + w2).to_int(), w, h);
             set_px(dst, v, x, y, w, h);
         }
     }
@@ -165,19 +186,19 @@ fn main() -> ! {
     let mut im_raw_rot: [u8; (H * W8) as usize] = [0; (H * W8) as usize];
 
     // Wait for the timer to trigger an update and change the state of the LED
-    let mut timer = Timer::syst(cp.SYST, 8.hz(), clocks);
+    //let mut timer = Timer::syst(cp.SYST, 8.hz(), clocks);
     let mut rot = 0;
     loop {
-        //hprintln!("loop {}", rot).unwrap();
-        block!(timer.wait()).unwrap();
-        led.set_high();
-        block!(timer.wait()).unwrap();
+        // hprintln!("loop {}", rot).unwrap();
+        // block!(timer.wait()).unwrap();
+        // block!(timer.wait()).unwrap();
         led.set_low();
         rotate(&mut im_raw_rot, &im_raw, W, H, rot);
         let im = Image1BPP::new(&im_raw_rot[..], H, W);
         disp.draw(im.translate(Coord::new(32, 0)).into_iter());
         disp.flush().unwrap();
-        rot += 8;
+        led.set_high();
+        rot += 2;
     }
 }
 
